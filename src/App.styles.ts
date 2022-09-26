fe47fa1966e247f8b2acfc09161dd146ad7d2231
RONALDOPEREIRADASILVA <eletrotecnica.ronaldo@gmail.com>
2022-09-26 08:42:01 -0300

    Atualização

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,9 +1,18 @@
 import styled from'styled-components';
 
-export const cotainer = styled.div``
+export const container = styled.div``;
 
-export const Header = styled.div``
+export const Header = styled.div`
+    background-color:darkblue;
+    height:150px;
+    text-align:center;
+`;
 
-export const HeaderText = styled.div``
+export const HeaderText = styled.h1`
+    margin:0;
+    padding:0;
+    color:#FFF;
+    padding-top:30px;
+`;
 
-export const body = styled.div``
+export const Body = styled.div``;
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as c from'./App.styles';
+
+const App = () =>{
+  return (
+    <c.container>
+      <c.Header>
+          <c.HeaderText>Sistema Financeiro</c.HeaderText>
+
+      </c.Header>
+      <c.Body>
+
+      </c.Body>
+
+    </c.container>
+
+  );
+
+}
+
+  
+export default App;
