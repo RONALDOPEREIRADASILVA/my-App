a7674e2af574d41051b9620c129871779d17c2c6
RONALDOPEREIRADASILVA <eletrotecnica.ronaldo@gmail.com>
2022-09-25 23:14:42 -0300

    Treinando criando App

---
+++ src/App.styles.ts
@@ -0,0 +1,9 @@
+import styled from'styled-components';
+
+export const cotainer = styled.div``
+
+export const Header = styled.div``
+
+export const HeaderText = styled.div``
+
+export const body = styled.div``
